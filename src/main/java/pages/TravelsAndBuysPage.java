97af894c6bcab19f828c8893c1624f87cff377f5
Andrew-Bear-cub <andrew.bearcub@gmail.com>
2018-10-31 16:35:20 +0300

    Второе задание: 1.  Переписать тест с использованием PageObject и PageFactory
                    2.  Вынести в environment.properties переменные:
    
                    1. Браузер, в котором запускаются тесты
    
                    2. Путь к драйверу браузера
    
                    3. Настроить запуск тестов с использованием maven
    
                    4. Выделить класс BaseTest,  методы которого могут быть переиспользованы в дргуих тестах

---
+++ src/main/java/pages/TravelsAndBuysPage.java
@@ -0,0 +1,22 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TravelsAndBuysPage {
+    @FindBy(xpath = "//div[@data-pid='SBRF_ColList_sb_bundle-9624889']//*/p[@class='kit-button kit-button_color_green kit-button_size_m']")
+    public WebElement sendButton;
+
+    public TravelsAndBuysPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        //Ожидание появления кнопки
+        Wait<WebDriver> wait = new WebDriverWait(driver, 5, 1000);
+        wait.until(ExpectedConditions.visibilityOf(sendButton)).click();
+
+    }
+}
